222b6b0d12fb2660ab856b2f8420ca6a47a53dce
Vinko90 <33526702+Vinko90@users.noreply.github.com>
2023-05-02 20:40:41 +0200

    Style header bar

--- src/Skinet.Web/ClientApp/src/app/app.module.ts
+++ src/Skinet.Web/ClientApp/src/app/app.module.ts
@@ -6,29 +6,31 @@ import { RouterModule } from '@angular/router';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'counter', component: CounterComponent},
+      {path: 'fetch-data', component: FetchDataComponent},
+    ]),
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
