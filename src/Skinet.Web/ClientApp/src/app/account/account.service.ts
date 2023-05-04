681f49e16bb4e414a8ed588fe4baff467865546e
Vinko90 <33526702+Vinko90@users.noreply.github.com>
2023-05-04 19:45:37 +0200

    Account components

---
+++ src/Skinet.Web/ClientApp/src/app/account/account.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, map} from "rxjs";
+import {User} from "../shared/models/user.model";
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  public currentUserSource$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(values: any) {
+    return this.http.post<User>('account/login', values).pipe(
+      map(user => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.http.post<User>('account/register', values).pipe(
+      map(user => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string) {
+    return this.http.get<boolean>('account/emailExists?email=' + email);
+  }
+}
