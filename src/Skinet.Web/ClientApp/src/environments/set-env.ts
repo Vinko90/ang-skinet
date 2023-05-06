a83df264b18a8b9336753686d7f178427d7e89ca
Vinko90 <33526702+Vinko90@users.noreply.github.com>
2023-05-06 13:57:02 +0200

    Add script for secrets

---
+++ src/Skinet.Web/ClientApp/src/environments/set-env.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+  // Configure Angular dev and prod `environment.ts` file paths
+  const devTargetPath = './src/environments/environment.ts';
+  const prodTargetPath = './src/environments/environment.prod.ts';
+  //Hidden file where to get keys
+  require('dotenv').config({
+    path: 'src/environments/.env'
+  });
+
+  // ****** Development File ******
+  const devEnvConfigFile = `export const environment = {
+      production: false,
+      stripePubKey: '${process.env.DEV_STRIPE_PUB_KEY}',
+    };`;
+
+  console.log('The dev file `environment.ts` will be written with the following content: \n');
+  writeFile(devTargetPath, devEnvConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(`Angular Dev environment.ts file generated correctly at ${devTargetPath} \n`);
+    }
+  });
+
+
+  // ****** Production File ******
+  const prodEnvConfigFile = `export const environment = {
+      production: true,
+      stripePubKey: '${process.env.PROD_STRIPE_PUB_KEY}',
+    };`;
+
+  console.log('The production file `environment.prod.ts` will be written with the following content: \n');
+  writeFile(prodTargetPath, prodEnvConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(`Angular Production environment.prod.ts file generated correctly at ${prodTargetPath} \n`);
+    }
+  });
+};
+
+setEnv();
